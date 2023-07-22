20a7ec062a9c454979bdf491447fd8a3dca4676c
ewen <66205491+ewenman5137@users.noreply.github.com>
2023-07-22 16:20:19 +0200

    correction bouton titre

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
+        var headerOffset = 80;  // hauteur de la barre de navigation
+        var elementPosition = document.querySelector(this.getAttribute('href')).offsetTop;
+        var offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
             behavior: 'smooth'
         });
     });
